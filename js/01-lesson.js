381fc753f77ecd4f19d01ba8bf0a15900bb8ee9e
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-02-11 10:25:16 +0200

    trial 1

--- js/01-lesson.js
+++ js/01-lesson.js
@@ -1,25 +1,8 @@
-//  1. сделать переменные
-const option = 1526;
-let message = '';
+//Вывести первых 10 четных чисел
+console.log(5);
 
-// 2. сделать switch 1 2 3
-// 3. в каждом кейсе записать в message строку
-switch (option) {
-  case 1:
-    message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
-    break;
-
-  case 2:
-    message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
-    break;
-
-  case 3:
-    message = 'Посылка будет отправлена сегодня';
-    break;
-
-  default:
-    message = 'Вам перезвонит менеджер';
-}
-
-// 4. сделать лог message
-console.log(message);
+// for (let i = 0; i <= 20; i += 1) { 
+//   if (i % 2 === 0) {
+//     console.log(i)
+//   }
+// }
