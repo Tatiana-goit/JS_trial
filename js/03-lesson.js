89681946309dc4cbb1ebf3c2e2d60c18c8591b45
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-02-24 14:26:33 +0200

    Решение задач

--- js/03-lesson.js
+++ js/03-lesson.js
@@ -24,3 +24,182 @@
 
 // schoolClass.newPupil('Симоненко', 'Катя');
 // console.log(schoolClass);
+
+
+// Задачи 3 модуль
+// 1-2. Присвой переменной apartment объект описывающий квартиру со следующими характеристиками
+//  Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. :
+
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//     owner: {
+//         name: 'Генри',
+//         phone: '982-126-1588',
+//         email: 'henry.carter@aptmail.com',
+//     },
+// };
+
+// console.log(apartment);
+
+
+// 3. 5 Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//   };
+  
+//   const aptRating = apartment.rating;
+//   const aptDescr = apartment.descr;
+//   const aptPrice = apartment.price;
+//   const aptTags = apartment.tags;
+
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+
+// 4. Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+// ownerName - имя владельца;
+// ownerPhone - телефон владельца;
+// ownerEmail - почта владельца;
+// numberOfTags - количество элементов массива в свойстве tags;
+// firstTag - первый элемент массива в свойстве tags;
+// lastTag - последний элемент массива в свойстве tags.
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length-1];
+
+// console.log(ownerName);
+// console.log(ownerPhone);
+// console.log(ownerEmail);
+// console.log(numberOfTags);
+// console.log(firstTag);
+// console.log(lastTag);
+
+
+
+// 6-7. Дополни код обновив значения свойств объекта apartment:
+
+// цену в свойстве price на 5000;
+// рейтинг квартиры в свойстве rating на 4.7;
+// имя владельца во вложенном свойстве name на 'Генри Сибола';
+// массив тегов в свойстве tags добавив в конец строку 'trusted'.
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com'
+//   }
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Генри Сибола';
+// apartment.tags.push('trusted');
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Ямайка';
+// apartment.location.city = 'Кингстон';
+
+
+// console.log(apartment);
+
+
+// 8. Дополни код объявления объекта так, чтобы у него были свойства name, price, image и tags со значениями из переменных с аналогичными именами. Обязательно используй синтаксис коротких свойств.
+
+// const name = 'Ремонтный дроид';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//     name,
+//     price,
+//     image,
+//     tags,
+// };
+
+// console.log(product);
+
+
+// 9. Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
+
+// Значением свойства email должна быть строка 'henry.carter@aptmail.com', а значением свойства password - строка 'jqueryismyjam'.
+
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//     [emailInputName]: 'henry.carter@aptmail.com',
+//     [passwordInputName]: 'jqueryismyjam'
+
+// };
+
+// console.log(credentials);
+
+// 10. Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//     keys.push(key);
+//     values.push(apartment[key])
+// }
+// console.log(keys);
+// console.log(values);
+
+
+!!!! ИЛИ
+
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+
+// console.log(Object.keys(apartment));
+
+// console.log(Object.values(apartment));
