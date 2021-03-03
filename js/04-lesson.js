8cd8b9bb9857213ddb2d89de1135f78242c1f6db
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-03-03 14:11:00 +0200

    Задачи

--- js/04-lesson.js
+++ js/04-lesson.js
@@ -98,3 +98,212 @@
 // fnB();
 
 
+
+
+
+// 1. Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza.
+
+// function makePizza() {
+//   return 'Ваша пицца готовится, ожидайте.';
+// }
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(result);
+// console.log(pointer);
+
+
+
+
+
+// 2. Дополни функцию makeMessage так, чтобы она ожидала вторым параметром (параметр callback) колбэк-функцию и возвращала ее вызов. Функция deliverPizza или makePizza будет передаваться как колбэк и ожидать аргументом имя готовой доставляемой пиццы.
+
+// !!!!!!! из образца
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+
+// Для автопроверки
+// registerGuest('Манго', name => console.log(`Добро пожаловать ${name}.`));
+
+// function deliverPizza(pizzaName) {
+//   return `Доставляем пиццу ${pizzaName}.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Пицца ${pizzaName} готовится, ожидайте...`;
+// }
+
+// function makeMessage(pizzaName, callback) {
+//   return  callback(pizzaName) ;
+// }
+
+// console.log(makeMessage('Роял гранд', makePizza));
+// console.log(makeMessage('Ультрасыр', deliverPizza));
+
+
+// 2. способ
+//   function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName) ;
+// }
+
+// console.log(makeMessage('Роял гранд', pizza => `Пицца ${pizza} готовится, ожидайте...`));
+// console.log(makeMessage('Ультрасыр', pizza => `Доставляем пиццу ${pizza}.`));
+
+
+
+
+
+// 3. Дополни второй вызов функции makePizza(pizzaName, callback), передав вторым аргументом инлайн колбэк-функцию eatPizza(pizzaName), которая логирует строку 'Едим пиццу <имя пиццы>'.
+
+// !!!!!! из образца
+// const registerGuest = (name, callback) => {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest('Манго', name => console.log(`Добро пожаловать ${name}.`));
+// registerGuest('Поли', name => console.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`));
+
+
+// Для автопроверки
+// function makePizza(pizzaName, callback) {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza('Роял гранд', function deliverPizza(pizzaName) {
+//   console.log(`Доставляем пиццу ${pizzaName}.`);
+// });
+
+// makePizza('Ультрасыр', function eatPizza(pizzaName) {
+//   console.log(`Едим пиццу ${pizzaName}.`);
+// });
+
+
+// 2 способ
+// const makePizza = (pizzaName, callback) => {
+//   console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+//   callback(pizzaName);
+// }
+
+// makePizza('Роял гранд', pizzaName => console.log(`Доставляем пиццу ${pizzaName}.`));
+// makePizza('Ультрасыр', pizzaName => console.log(`Едим пиццу ${pizzaName}.`));
+
+
+
+
+
+// 4. ниже
+
+// !!!из образца
+// function processCall(recipient) {
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступен, оставьте сообщение.`);
+//   } else {
+//     console.log(`Соединяем с ${recipient}, ожидайте...`);
+//   }
+// }
+
+// processCall('Манго');
+
+
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Имитируем доступеность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+//   // Логика принятия звонка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+//   // Логика активации автоответчика
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+//   // Логика записи голограммы
+// }
+
+// processCall('Манго', takeCall, activateAnsweringMachine);
+// processCall('Поли', takeCall, leaveHoloMessage);
+
+
+
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+//   onAvailable(recipient);
+// }
+
+// processCall('Манго',name => console.log(`Соединяем с ${name}, ожидайте...`),name => console.log(`Абонент ${name} недоступен, оставьте сообщение.`));
+// processCall('Поли', name => console.log(`Соединяем с ${name}, ожидайте...`),name => console.log(`Абонент ${name} недоступен, записываем голограмму.`));
+
+
+
+// 4. Необходимо написать логику обработки заказа пиццы. Выполни рефакторинг метода order так, чтобы он принимал вторым и третим параметрами два колбэка onSuccess и onError.
+// Если в свойстве pizzas нет пиццы с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onError, передавая ему аргументом строку 'В ассортименте нет пиццы с названием <имя пиццы>.'
+// Если в свойстве pizzas есть пицца с названием из параметра pizzaName, метод order должен возвращать результат вызова колбэка onSuccess, передавая ему аргументом имя заказанной пиццы.
+// После объявления объекта pizzaPalace мы добавили колбэки и вызовы методов.Пожалуйста ничего там не меняй.
+
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       console.log(onSuccess(pizzaName));
+//     } else { 
+//       console.log(onError(pizzaName));
+//     }  
+//   },
+// };
+
+
+
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return onSuccess(pizzaName);
+//     } 
+//     return onError(pizzaName);
+//   },
+// };
+
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// function onOrderError(error) {
+//   return `Ошибка! В ассортименте нет пиццы с названием ${error}.`;
+// }
+
+// pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
+// pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+// pizzaPalace.order('Биг майк', makePizza, onOrderError);
+// pizzaPalace.order('Венская', makePizza, onOrderError);
+
+// console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Биг майк', makePizza, onOrderError) );
+
+
+
