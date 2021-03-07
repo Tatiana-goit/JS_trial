a065db32f36fd2b7b50df4e072c8a6bfb26a1399
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-03-07 14:38:36 +0200

    Задачи

--- js/04-lesson.js
+++ js/04-lesson.js
@@ -333,26 +333,26 @@
 
 // Выполни рефакторинг методов объекта pizzaPalace, расставив отсутствующие this в местах обращения к свойствам и методам объекта.
 
-// const pizzaPalace = {
-//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
 
-//   checkPizza(pizzaName) {
-//     return this.pizzas.includes(pizzaName);
-//     },
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+    },
   
-//   order(pizzaName) {
-//     const isPizzaAvailable = this.checkPizza(pizzaName);
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
 
-//     if (!isPizzaAvailable) {
-//       return `В ассортименте нет пиццы с названием «${pizzaName}».`;
-//     }
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
 
-//     return `Заказ принят, готовим пиццу «${pizzaName}».`;
-//   },
-// };
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+};
 
-// console.log(pizzaPalace.order('Аль Копчино'));
-// console.log(pizzaPalace.order('Венская'));
+console.log(pizzaPalace.order('Аль Копчино'));
+console.log(pizzaPalace.order('Венская'));
 
 
 
@@ -478,55 +478,55 @@
 
 // 10. 
 
-const customer = {
-  firstName: 'Jacob',
-  lastName: 'Mercer',
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
 
-function makeMessage(callback) {
-  // callback() это вызов метода getFullName без объекта
-  console.log(`Обрабатываем заявку от ${callback()}.`);
-}
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
 
-makeMessage(customer.getFullName.bind(customer)); // Будет ошибка при вызове функции
+// makeMessage(customer.getFullName.bind(customer)); // Будет ошибка при вызове функции
 
 
-// Сервису рассылки электронной почты необходимо добавить логирование действий для сбора статистики. Функция logAndInvokeAction(email, action) ожидает почту и действие которое нужно выполнить - ссылку на метод объекта service. Сбор статистики симулируется логированием строки. Разберись и дополни код так, чтобы он работал верно.
+// // Сервису рассылки электронной почты необходимо добавить логирование действий для сбора статистики. Функция logAndInvokeAction(email, action) ожидает почту и действие которое нужно выполнить - ссылку на метод объекта service. Сбор статистики симулируется логированием строки. Разберись и дополни код так, чтобы он работал верно.
 
-const service = {
-  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
-  subscribe(email) {
-    this.mailingList.push(email);
-    return `Почта ${email} добавлена в рассылку.`;
-  },
-  unsubscribe(email) {
-    this.mailingList = this.mailingList.filter((e) => e !== email);
-    return `Почта ${email} удалена из рассылки.`;
-  },
-};
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter((e) => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
 
-function logAndInvokeAction(email, action) {
-  console.log(`Выполняем действие с ${email}.`);
-  return action(email);
-}
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
 
-const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe);
-console.log(firstInvoke);
-// Почта kiwi@mail.uk добавлена в рассылку.
+// const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe);
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
 
-console.log(service.mailingList);
-/* ['mango@mail.com', 
-    'poly@hotmail.de', 
-    'ajax@jmail.net', 
-    'kiwi@mail.uk']*/
-const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe);
-console.log(secondInvoke);
-// Почта poly@hotmail.de удалена из рассылки.
+// console.log(service.mailingList);
+// /* ['mango@mail.com', 
+//     'poly@hotmail.de', 
+//     'ajax@jmail.net', 
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe);
+// console.log(secondInvoke);
+// // Почта poly@hotmail.de удалена из рассылки.
 
-console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
 
 
 
